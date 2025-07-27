d64b4c7baa30672fe5a2e9764954173bcf4e8325
Amaterasvv <ktbobas@gmail.com>
2025-07-27 10:59:10 +0200

    Popravljen mobilni hamburger meni i stilovi

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-  function toggleMenu() {
-      const menu = document.getElementById("mobile-menu");
-      menu.classList.toggle("show");
-    }
+function toggleMenu() {
+  document.querySelector('.nav-left').classList.toggle('active');
+  document.querySelector('.nav-right').classList.toggle('active');
+  document.querySelector('.menu-toggle').classList.toggle('open');
+}
 
 
 const sadrzaji = {
